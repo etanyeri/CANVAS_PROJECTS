2b130162dc9a623a8fe16712fb731f94d87ba9e6
etanyeri <94400650+etanyeri@users.noreply.github.com>
2021-12-20 08:15:51 -0700

    'X' mark

---
+++ 'X'MarkSpot.py
@@ -0,0 +1,9 @@
+"""This program will draw an 'X' on canvas eith Tracy."""
+
+left(45)    # let it get her right angle first.
+for i in range(4): #make her repeat these mover 4 times
+    forward(100)
+    pendown()
+    backward(100)
+    penup()
+    left(90)
