e89db8e20d3af46164223c074c64c73870d954d8
etanyeri <94400650+etanyeri@users.noreply.github.com>
2021-12-22 01:30:02 -0700

    Circle Pyramid

---
+++ CIRCLE.PYRAMID.py
@@ -0,0 +1,27 @@
+
+
+""" This program will draw a pyramid of circles with 3 circles
+on the bottom row, 2 on the middle row, and 1 on the top."""
+
+
+speed(9)
+penup()
+setposition(-100,-200)
+pendown()
+for i in range(3): # make Tracy complete this 3 times
+    circle(50)  # Circle's radius should be 50 pixel.
+    penup()
+    forward(100)
+    pendown()
+penup()
+setposition(0,0)
+pendown()
+circle(50)
+penup()
+setposition(-50,-100)
+pendown()
+for i in range(2): #complete the middle 2 circles at last.
+    circle(50)
+    penup()
+    forward(100)
+    pendown()
